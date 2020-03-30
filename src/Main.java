b887841c3ca5f1f253d036c1e9d982c5df38d6b0
Aviran <avirangi@post.bgu.ac.il>
2020-03-30 15:37:46 +0300

    add x var

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int x=5;
+        System.out.println("the number is:"+x);
+
     }
 }
